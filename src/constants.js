cdb7baf1c101e5fab63b8b19441f1f4234accaa3
Hamza Kamran <hamzakamran@Hamzas-MacBook-Air.local>
2023-09-26 22:57:18 -0500

    Update backend endpoint

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,2 @@
 // export const URL = "http://localhost:3000";
-export const URL =
-	"http://mydry30-backend-env.eba-rhhsbmtp.us-west-1.elasticbeanstalk.com/";
+export const URL = "https://md30-rest-api-6f22f41d1519.herokuapp.com";
