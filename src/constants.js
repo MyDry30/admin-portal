c6b3e85cac045741e9ed9ac57ed077b5b9d2ad9b
hamzakamran <hamzakwebdev@gmail.com>
2023-12-08 02:03:34 -0600

    url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,6 @@
-export const URL = "https://md30-rest-api-6f22f41d1519.herokuapp.com";
+export const URL =
+	"http://mydry30-frontend-env.eba-rhhsbmtp.us-west-1.elasticbeanstalk.com";
+// https://md30-rest-api-6f22f41d1519.herokuapp.com";
 export const INITIAL_STATE = {
 	pagination: {
 		paginationModel: { page: 0, pageSize: 10 },
